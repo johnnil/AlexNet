3a8d434b00a07357dc09c6794a56d8e0c5a457c7
John Nilsson <chrysaries@hotmail.com>
2020-05-06 16:03:17 +0200

    Last 3 layers

--- alexnet.py
+++ alexnet.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-from tensorflow.keras import Model, datasets, layers, models, initializers
+from tensorflow.keras import Model, datasets, layers, models, initializers, losses
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
 import matplotlib.pyplot as plt
@@ -14,7 +14,7 @@ def load_dataset():
     return (train_images, train_labels), (test_images, test_labels)
 
 
-def add_conv_layers(model, image_size):
+def add_conv_layers(model, image_size, dropout=False):
 
     # Initialization: All layers are inittialized with normal dist. standard deviation=0.01 as said in report.
     # Padding: Not explained clear in report, I looked at a bunch of implementations and used to most common for each layer.
@@ -40,6 +40,30 @@ def add_conv_layers(model, image_size):
     model.add(Conv2D(filters=256, kernel_size=(3,3), strides= 1, padding= 'same', activation= 'relu', kernel_initializer=initializers.RandomNormal(stddev=0.01)))
     # Max pooling of 5th layer
     model.add(MaxPooling2D(pool_size=(3,3), strides= (2,2), padding= 'valid'))
+
+    # 6th layer, "Unroll" 3D input to 1D
+    model.add(Flatten())
+
+    # 7th layer (fully connected)
+    model.add(Dense(4096, input_shape=(224*224*3), activation='relu'))
+    if dropout:
+        model.add(Dropout(0.4))
+
+    # 8th layer (fully connected)
+    model.add(layers.Dense(4096, activation='relu'))
+    if dropout:
+        model.add(Dropout(0.4))
+
+    # 9th layer (fully connected)
+    model.add(layers.Dense(10, activation='relu')) # number of classes = 10
+    if dropout:
+        model.add(Dropout(0.4))
+
+    # 10th layer (one output for each class)
+    model.add(layers.Dense(10, activation='softmax'))
+
+    # Compile the model
+    model.compile(loss=losses.categorical_crossentropy, optimizer='adam', metrics=["accuracy"])
 
 
 load_dataset()
